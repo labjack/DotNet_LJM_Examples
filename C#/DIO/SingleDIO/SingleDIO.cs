c36f08ae30318ed92d2cf6a46596659d5b74efdc
David Lopez <david@labjack.com>
2016-11-03 17:37:13 -0600

    Updated DIO examples for T4 support.

--- C#/DIO/SingleDIO/SingleDIO.cs
+++ C#/DIO/SingleDIO/SingleDIO.cs
@@ -8,6 +8,7 @@
 using System;
 using LabJack;
 
+
 namespace SingleDIO
 {
     class SingleDIO
@@ -34,31 +35,61 @@ namespace SingleDIO
             int port = 0;
             int maxBytesPerMB = 0;
             string ipAddrStr = "";
+            string name = "";
+            double state = 0;
 
             try
             {
                 //Open first found LabJack
-                LJM.OpenS("ANY", "ANY", "ANY", ref handle);
-                //devType = LJM.CONSTANTS.dtANY; //Any device type
-                //conType = LJM.CONSTANTS.ctANY; //Any connection type
-                //LJM.Open(devType, conType, "LJM_idANY", ref handle);
-                
+                LJM.OpenS("ANY", "ANY", "ANY", ref handle);  // Any device, Any connection, Any identifier
+                //LJM.OpenS("T7", "ANY", "ANY", ref handle);  // T7 device, Any connection, Any identifier
+                //LJM.OpenS("T4", "ANY", "ANY", ref handle);  // T4 device, Any connection, Any identifier
+                //LJM.Open(LJM.CONSTANTS.dtANY, LJM.CONSTANTS.ctANY, "ANY", ref handle);  // Any device, Any connection, Any identifier
+
                 LJM.GetHandleInfo(handle, ref devType, ref conType, ref serNum, ref ipAddr, ref port, ref maxBytesPerMB);
                 LJM.NumberToIP(ipAddr, ref ipAddrStr);
                 Console.WriteLine("Opened a LabJack with Device type: " + devType + ", Connection type: " + conType + ",");
                 Console.WriteLine("Serial number: " + serNum + ", IP address: " + ipAddrStr + ", Port: " + port + ",");
                 Console.WriteLine("Max bytes per MB: " + maxBytesPerMB);
 
-                
                 //Setup and call eWriteName to set the DIO state.
-                string name = "FIO0";
-                double state = 1; //Output-low = 0, Output-high = 1
+                if(devType == LJM.CONSTANTS.dtT4)
+                {
+                    //Setting FIO4 on the LabJack T4. FIO0-FIO3 are reserved for
+                    //AIN0-AIN3.
+                    name = "FIO4";
+
+                    //If the FIO/EIO line is an analog input, it needs to first
+                    //be changed to a digital I/O by reading from the line or
+                    //setting it to digital I/O with the DIO_ANALOG_ENABLE
+                    //register.
+                    //For exanple:
+                    //LJM.eReadName(handle, name, ref state);
+                }
+                else
+                {
+                    //Setting FIO0 on the LabJack T7 and other devices.
+                    name = "FIO0";
+                }
+                state = 1;  //Output-low = 0, Output-high = 1
                 LJM.eWriteName(handle, name, state);
 
                 Console.WriteLine("\nSet " + name + " state : " + state);
 
                 //Setup and call eReadName to read the DIO state.
-                name = "FIO1";
+                if(devType == LJM.CONSTANTS.dtT4)
+                {
+                    //Reading from FIO5 on the LabJack T4. FIO0-FIO3 are
+                    //reserved for AIN0-AIN3.
+                    //Note: Reading a single digital I/O will change the line
+                    //from analog to digital input.
+                    name = "FIO5";
+                }
+                else
+                {
+                    //Reading from FIO1 on the LabJack T7 and other devices.
+                    name = "FIO1";
+                }
                 state = 0;
                 LJM.eReadName(handle, name, ref state);
 
@@ -69,11 +100,10 @@ namespace SingleDIO
                 showErrorMessage(e);
             }
 
-            LJM.CloseAll(); //Close all handles
+            LJM.CloseAll();  //Close all handles
 
             Console.WriteLine("\nDone.\nPress the enter key to exit.");
-            Console.ReadLine(); // Pause for user
+            Console.ReadLine();  // Pause for user
         }
-
     }
 }
--- VB/DIO/SingleDIO/SingleDIO.vb
+++ VB/DIO/SingleDIO/SingleDIO.vb
@@ -9,6 +9,7 @@ Option Explicit On
 
 Imports LabJack
 
+
 Module SingleDIO
 
     Sub showErrorMessage(ByVal e As LJM.LJMException)
@@ -35,28 +36,66 @@ Module SingleDIO
         Console.WriteLine("Max bytes per MB: " & maxBytesPerMB)
     End Sub
 
+    Function getDeviceType(ByVal handle As Integer)
+        Dim devType As Integer
+        Dim conType As Integer
+        Dim serNum As Integer
+        Dim ipAddr As Integer
+        Dim port As Integer
+        Dim maxBytesPerMB As Integer
+
+        LJM.GetHandleInfo(handle, devType, conType, serNum, ipAddr, port, _
+                          maxBytesPerMB)
+        Return devType
+    End Function
+
     Sub Main()
         Dim handle As Integer
         Dim name As String
         Dim state As Double
+        Dim devType As Integer
 
         Try
             ' Open first found LabJack
-            LJM.OpenS("ANY", "ANY", "ANY", handle)
-            'LJM.Open(LJM.CONSTANTS.dtANY, LJM.CONSTANTS.ctANY, "ANY", handle)
+            LJM.OpenS("ANY", "ANY", "ANY", handle)  ' Any device, Any connection, Any identifier
+            'LJM.OpenS("T7", "ANY", "ANY", handle)  ' T7 device, Any connection, Any identifier
+            'LJM.OpenS("T4", "ANY", "ANY", handle)  ' T4 device, Any connection, Any identifier
+            'LJM.Open(LJM.CONSTANTS.dtANY, LJM.CONSTANTS.ctANY, "ANY", handle)  ' Any device, Any connection, Any identifier
 
             displayHandleInfo(handle)
+            devType = getDeviceType(handle)
 
-            ' Setup and call eWriteName to set the DIO state.
-            name = "FIO0"
-            state = 1 ' Output-low = 0, Output-high = 1
+            If devType = LJM.CONSTANTS.dtT4 Then
+                ' Setting FIO4 on the LabJack T4. FIO0-FIO3 are reserved for
+                ' AIN0-AIN3.
+                name = "FIO4"
+
+                ' If the FIO/EIO line is an analog input, it needs to first be
+                ' changed to a digital I/O by reading from the line or setting
+                ' it to digital I/O with the DIO_ANALOG_ENABLE register.
+                ' For exanple:
+                'LJM.eReadName(handle, name, state)
+            Else
+                ' Setting FIO0 on the LabJack T7 and other devices.
+                name = "FIO0"
+            End If
+            state = 1  ' Output-low = 0, Output-high = 1
             LJM.eWriteName(handle, name, state)
 
             Console.WriteLine("")
             Console.WriteLine("Set " & name & " state : " & state)
 
             ' Setup and call eReadName to read the DIO state.
-            name = "FIO1"
+            If devType = LJM.CONSTANTS.dtT4 Then
+                ' Reading from FIO5 on the LabJack T4. FIO0-FIO3 are reserved
+                ' for AIN0-AIN3.
+                ' Note: Reading a single digital I/O will change the line from
+                ' analog to digital input.
+                name = "FIO5"
+            Else
+                ' Reading from FIO1 on the LabJack T7 and other devices.
+                name = "FIO1"
+            End If
             state = 0
             LJM.eReadName(handle, name, state)
 
@@ -66,12 +105,12 @@ Module SingleDIO
             showErrorMessage(ljme)
         End Try
 
-        LJM.CloseAll() ' Close all handles
+        LJM.CloseAll()  ' Close all handles
 
         Console.WriteLine("")
         Console.WriteLine("Done.")
         Console.WriteLine("Press the enter key to exit.")
-        Console.ReadLine() ' Pause for user
+        Console.ReadLine()  ' Pause for user
     End Sub
 
 End Module
